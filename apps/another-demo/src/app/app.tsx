39593bde0617a9fe19c933dd14c5c4a0e275401c
Ankush Kalia <ankush.kalia@ihsmarkit.com>
2023-07-19 20:47:05 +0530

    feat(another-demo): :sparkles: added shared UI component in Another demo

--- apps/another-demo/src/app/app.tsx
+++ apps/another-demo/src/app/app.tsx
@@ -3,12 +3,14 @@ import styles from './app.module.css';
 import { DemoFeatureLib } from '@semver/demo/feature-lib';
 
 import NxWelcome from './nx-welcome';
+import { SharedUi } from '@semver/shared/ui';
 
 export function App() {
   return (
     <div>
       <NxWelcome title="another-demo" />
       <DemoFeatureLib />
+      <SharedUi />
     </div>
   );
 }
